0098d8e8d4d2dc9faee2ec22128795e113dc138e
Zecong Hu <huzecong@gmail.com>
2019-01-07 22:57:07 -0500

    Add hack for readthedocs

--- nnlib/torch.py
+++ nnlib/torch.py
@@ -1,13 +1,28 @@
+import os
 from typing import Tuple
 
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-import torch.nn.utils
-# noinspection PyUnresolvedReferences
-from torch import Tensor, LongTensor
-from torch.autograd import Variable
-from torch.nn.utils.rnn import PackedSequence
+if 'READTHEDOCS' in os.environ:
+    class _Dummy:
+        def __getattr__(self, item):
+            if ord('A') <= ord(item[0]) <= ord('Z'):
+                return _Dummy
+            else:
+                return _Dummy()
+
+    # since torch is not installed on readthedocs, we just mock them
+    for name in ['torch', 'nn', 'F', 'utils']:
+        globals()[name] = _Dummy()
+    for typ in ['Tensor', 'LongTensor', 'Variable', 'PackedSequence']:
+        globals()[typ] = _Dummy
+else:
+    import torch
+    import torch.nn as nn
+    import torch.nn.functional as F
+    import torch.nn.utils
+    # noinspection PyUnresolvedReferences
+    from torch import Tensor, LongTensor
+    from torch.autograd import Variable
+    from torch.nn.utils.rnn import PackedSequence
 
 STATICA_HACK = True
 globals()['kcah_acitats'[::-1].upper()] = False  # global['STATICA_HACK'] = False
