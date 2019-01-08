0362511c993f47bba75819f0ddac70ed49e43a4a
Zecong Hu <huzecong@gmail.com>
2019-01-07 21:43:18 -0500

    Add __init__.py for easy import as a submodule

---
+++ __init__.py
@@ -0,0 +1,7 @@
+from nnlib import arguments
+from nnlib import data
+from nnlib import modules
+from nnlib import torchutils
+from nnlib import train
+from nnlib import utils
+from nnlib import torch
